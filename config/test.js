b99278e66beb623ce70c7d9d3df2ab902a35e5e2
AgustinRow <agus.diaz.11@gmail.com>
2022-06-14 20:21:35 -0300

    fix: improves on config variables

--- config/test.js
+++ config/test.js
@@ -1,12 +1,5 @@
 module.exports = {
-  port: 3300,
   mongodb: {
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    dbname: process.env.DATABASE_DBNAME,
-    address: process.env.DATABASE_HOST + ':' + process.env.DATABASE_PORT,
     url: process.env.MONGODB_URL_TEST,
   },
 };
