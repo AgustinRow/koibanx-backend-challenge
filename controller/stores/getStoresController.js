f6c278206b88a0b0270f5b736aad62efd4e89e25
AgustinRow <agus.diaz.11@gmail.com>
2022-06-15 18:38:59 -0300

    fix: on a variable name

--- controller/stores/getStoresController.js
+++ controller/stores/getStoresController.js
@@ -1,19 +1,18 @@
 const logger = require('../../utils/logger');
 
-
 const getStoresController =
   ({ getStores, decorateData }) =>
   async ({ page, limit }) => {
     try {
       const stores = await getStores({ page, limit });
-      const responseDecorted = {
+      const responseDecorated = {
         data: decorateData(stores.docs),
         page: stores.page,
         limit: stores.limit,
         pages: stores.totalPages,
         total: stores.totalDocs,
       };
-      return responseDecorted;
+      return responseDecorated;
     } catch (error) {
       logger.error(error);
       throw Error();
